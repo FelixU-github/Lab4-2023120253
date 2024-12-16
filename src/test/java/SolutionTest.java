a4fb5d7b659eb65597e2a38dc2d67aee97ccef9a
felixyuu <128110495+FelixU-github@users.noreply.github.com>
2024-12-17 00:53:50 +0800

    For testing auto merge workflow

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -17,6 +17,7 @@ public class SolutionTest {
         // 测试用例 3: 输入包含 "0"
         assertEquals("0", solution.multiply("0", "123"));
         assertEquals("0", solution.multiply("123", "0"));
+        assertEquals("0", solution.multiply("13", "0"));
 
         // 测试用例 4: 单位数相乘
         assertEquals("81", solution.multiply("9", "9"));
